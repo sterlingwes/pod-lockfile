991ad5f82e1c693f063f50fde5cc4298b202270d
Wes Johnson <wes@swj.io>
2024-09-14 17:05:01 -0400

    allow specific pod version install

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,46 @@
 import { execSync } from "child_process";
 import { existsSync } from "fs";
 
+const flags = process.argv.reduce((acc, arg, index) => {
+  const nextArg = process.argv[index + 1] ?? "";
+  if (arg.startsWith("--") && nextArg.startsWith("--") === false) {
+    return {
+      ...acc,
+      [arg.replace("--", "")]: nextArg || true,
+    };
+  }
+  return acc;
+}, {});
+
+const podVersion = flags["pod-version"];
+const requiresPodVersion = typeof podVersion === "string";
 const installedGems = execSync("gem list --local").toString();
 
 const podInstalled = installedGems.split(/\r?\n/).find((gem) => {
   return gem.trim().startsWith("cocoapods (");
 });
 
+if (
+  podInstalled &&
+  requiresPodVersion &&
+  podInstalled.includes(`(${podVersion.trim()})`) === false
+) {
+  throw new Error(
+    `Pod version mismatch, expected ${podVersion}, got:\n\t${podInstalled}\n`
+  );
+}
+
 if (!podInstalled) {
-  console.log("Installing cocoapods");
-  execSync("gem install cocoapods");
+  console.log(
+    requiresPodVersion
+      ? `Installing cocoapods w/ required version ${podVersion}`
+      : "Installing cocoapods"
+  );
+  execSync(
+    requiresPodVersion
+      ? `gem install cocoapods -v ${podVersion}`
+      : "gem install cocoapods"
+  );
 }
 
 const pluginInstalled = installedGems.split(/\r?\n/).find((gem) => {
